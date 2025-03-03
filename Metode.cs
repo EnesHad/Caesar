ef5f145db4ad029b9d04b9e8909b6322de334c01
Enes Hadžić <147485765+EnesHad@users.noreply.github.com>
2025-03-03 22:05:45 +0100

    Add files via upload

---
+++ Metode.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar
+{
+    class Metode
+    {
+        char[] abeceda = ['A', 'B', 'C', 'Č', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+        string en = "";
+        string de = "";
+        char c;
+        int vsotaZamika = 0;
+        public string encript(string niz, int zamik) {
+            niz = niz.ToUpper();
+            for (int i = 0; i < niz.Length; i++)
+            {
+                c = niz[i];
+                for (int k = 0; k <= 25; k++)
+                {
+                    if (c == abeceda[k])
+                    {
+                        vsotaZamika = k + zamik + 1;
+                        if (vsotaZamika > 25) vsotaZamika -= 25;
+                        en = en + abeceda[vsotaZamika];
+                        break;
+                    }
+                }
+            }
+            return en;
+
+        }
+
+        public string decode(string niz, int zamik) {
+            niz = niz.ToUpper();
+            for (int i = 0; i < niz.Length; i++)
+            {
+                c = niz[i];
+                for (int k = 0; k <= 25; k++)
+                {
+                    if (c == abeceda[k])
+                    {
+                        vsotaZamika = (k - zamik) - 1;
+                        if (vsotaZamika < 0) vsotaZamika += 25;
+                        de = de + abeceda[vsotaZamika];
+                        break;
+                    }
+                }
+            }
+            return de;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Caesar
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Metode met = new Metode();
+
+            string niz = "ABRAKADABRaz";
+            int zamik = 3;
+            string cripted = met.encript(niz, zamik);
+            Console.WriteLine("Enkodiranje niza: " + niz);
+            Console.WriteLine("Enkodirano v: " + cripted);
+
+            Console.WriteLine("\nDekodiranje niza: " + cripted);
+            Console.WriteLine("Enkodirano v: " + met.decode(cripted, zamik));
+
+        }
+    }
+}
