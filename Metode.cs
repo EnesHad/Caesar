a6002511875761281707aec3a3719818f0621fec
Enes Hadžić <147485765+EnesHad@users.noreply.github.com>
2025-03-13 13:50:47 +0100

    Add files via upload

--- Metode.cs
+++ Metode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,46 +9,91 @@ namespace Caesar
 {
     class Metode
     {
-        char[] abeceda = ['A', 'B', 'C', 'Č', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
+        char[] abcM = ['a', 'b', 'c', 'č', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 'š', 't', 'u', 'v', 'z', 'ž'];
+        char[] abcV = ['A', 'B', 'C', 'Č', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'Š', 'T', 'U', 'V', 'Z', 'Ž'];
         string en = "";
         string de = "";
         char c;
+        bool b;
+        string space = " ";
+        bool l;
         int vsotaZamika = 0;
+
+
         public string encript(string niz, int zamik) {
-            niz = niz.ToUpper();
+            //niz = niz.ToUpper();
             for (int i = 0; i < niz.Length; i++)
             {
                 c = niz[i];
-                for (int k = 0; k <= 25; k++)
-                {
-                    if (c == abeceda[k])
+                if (Char.IsWhiteSpace(c)) { en = en + space; continue; }
+                b = Char.IsUpper(c);
+                if ( b == true) {
+                    for (int k = 0; k <= 25; k++)
                     {
-                        vsotaZamika = k + zamik + 1;
-                        if (vsotaZamika > 25) vsotaZamika -= 25;
-                        en = en + abeceda[vsotaZamika];
-                        break;
+                        if (c == abcV[k])
+                        {
+                            vsotaZamika = k + zamik + 1;
+                            if (vsotaZamika > 25) vsotaZamika -= 25;
+                            en = en + abcV[vsotaZamika];
+                            break;
+                        }
+                    }
+                    b= false;
+                }
+                else
+                {
+                    for (int k = 0; k <= 25; k++)
+                    {
+                        if (c == abcM[k])
+                        {
+                            vsotaZamika = k + zamik + 1;
+                            if (vsotaZamika > 25) vsotaZamika -= 25;
+                            en = en + abcM[vsotaZamika];
+                            break;
+                        }
                     }
                 }
+                    
             }
             return en;
 
         }
 
         public string decode(string niz, int zamik) {
-            niz = niz.ToUpper();
+            //niz = niz.ToUpper();
             for (int i = 0; i < niz.Length; i++)
             {
                 c = niz[i];
-                for (int k = 0; k <= 25; k++)
+                if (Char.IsWhiteSpace(c)) { de = de + space; continue; }
+                l = Char.IsUpper(c);
+                if (l == true)
                 {
-                    if (c == abeceda[k])
+                    for (int k = 0; k <= 25; k++)
                     {
-                        vsotaZamika = (k - zamik) - 1;
-                        if (vsotaZamika < 0) vsotaZamika += 25;
-                        de = de + abeceda[vsotaZamika];
-                        break;
+                        if (c == abcV[k])
+                        {
+                            vsotaZamika = (k - zamik) - 1;
+                            if (vsotaZamika < 0) vsotaZamika += 25;
+                            de = de + abcV[vsotaZamika];
+                            break;
+                        }
+                    }
+                    l=false;
+
+                }
+                else { 
+                 for (int k = 0; k <= 25; k++)
+                    {
+                        if (c == abcM[k])
+                        {
+                            vsotaZamika = (k - zamik) - 1;
+                            if (vsotaZamika < 0) vsotaZamika += 25;
+                            de = de + abcM[vsotaZamika];
+                            break;
+                        }
                     }
                 }
+                   
             }
             return de;
         }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,14 @@ namespace Caesar
         {
             Metode met = new Metode();
 
-            string niz = "ABRAKADABRaz";
-            int zamik = 3;
+            Console.WriteLine("Vpiši text ki ga želiš zakodirati");
+            string niz = Console.ReadLine();
+            Console.WriteLine("\nVpiši številko zamika abecede");
+            int zamik = int.Parse(Console.ReadLine());
+
             string cripted = met.encript(niz, zamik);
-            Console.WriteLine("Enkodiranje niza: " + niz);
+
+            Console.WriteLine("\nEnkodiranje niza: " + niz);
             Console.WriteLine("Enkodirano v: " + cripted);
 
             Console.WriteLine("\nDekodiranje niza: " + cripted);
